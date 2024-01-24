0abc9c3f999b8191d5b8186bb6177cfc61576136
RiceQuakes <2486921393@qq.com>
2024-01-24 16:44:08 +0800

    add welcome

---
+++ src/main/java/lqw/plugintest/BasicRules/Welcome.java
@@ -0,0 +1,24 @@
+package lqw.plugintest.BasicRules;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Welcome implements Listener {
+    @EventHandler
+    public void welcomePlayer(PlayerJoinEvent event) {
+        for (Player player : event.getPlayer().getServer().getOnlinePlayers()) {
+            int onlineCount = event.getPlayer().getServer().getOnlinePlayers().size();
+            String subTitle = ChatColor.YELLOW + "Online Player Count: " + onlineCount;
+            if (player.getName().equals(event.getPlayer().getName())) {
+                player.sendTitle(ChatColor.GREEN + "Welcome!", subTitle, 10, 30, 10);
+            } else {
+                player.sendTitle(ChatColor.GREEN + event.getPlayer().getName() + " Joined!", subTitle, 10, 40, 20);
+            }
+            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1f, 1f);
+        }
+    }
+}
--- src/main/java/lqw/plugintest/PluginTest.java
+++ src/main/java/lqw/plugintest/PluginTest.java
@@ -1,5 +1,6 @@
 package lqw.plugintest;
 
+import lqw.plugintest.BasicRules.Welcome;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginTest extends JavaPlugin {
@@ -7,10 +8,15 @@ public final class PluginTest extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("PluginTest loaded");
+        registerEvents();
     }
 
     @Override
     public void onDisable() {
         getLogger().info("PluginTest unloaded");
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new Welcome(), this);
     }
 }
