22a51af736baac353593a71ed3c5d93b9bffe2ed
RiceQuakes <2486921393@qq.com>
2024-01-25 18:01:20 +0800

    code reformat
    optimized imports and make final

--- src/main/java/lqw/plugintest/BasicRules/SpawnSupply.java
+++ src/main/java/lqw/plugintest/BasicRules/SpawnSupply.java
@@ -5,16 +5,12 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.logging.Logger;
-
 public class SpawnSupply implements Listener {
-    private String[] supplies = new String[]{"COOKIE", "CLAY", "CLAY"};
-    private int[] suppliesNum = new int[]{0, 0, 0};
+    private final String[] supplies = new String[]{"COOKIE", "CLAY", "CLAY"};
+    private final int[] suppliesNum = new int[]{0, 0, 0};
 
     @EventHandler
     public void respawnSupply(PlayerPostRespawnEvent event) {
