39bdd2d95504748f4c7ca68485b424cb6f88f8e2
RiceQuakes <2486921393@qq.com>
2024-01-30 20:06:57 +0800

    fix movable

--- src/main/java/lqw/plugintest/Props/Movable.java
+++ src/main/java/lqw/plugintest/Props/Movable.java
@@ -3,7 +3,6 @@ package lqw.plugintest.Props;
 import lqw.plugintest.PluginTest;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,49 +15,49 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class Movable implements Listener {
-    private Set<String>set;
-    private static HashMap<Player, Entity> controlEn;
+    private Set<String> set;
+    private static HashMap<Player, Entity> controlEn = new HashMap<>();
     private PluginTest plugin;
 
-    public Movable(PluginTest pluginTest){
+    public Movable(PluginTest pluginTest) {
         plugin = pluginTest;
 
 //        add(EntityType.VILLAGER.name());
 
     }
 
-    public void add(String s){
+    public void add(String s) {
         set.add(s);
     }
 
 
     @EventHandler
     public void onPlayerRightClickEntity(PlayerInteractAtEntityEvent event) {
-        if(event.getHand() == EquipmentSlot.OFF_HAND)return;
+        if (event.getHand() == EquipmentSlot.OFF_HAND) return;
         Player player = event.getPlayer();
         Entity entity = event.getRightClicked();
-            player.sendMessage(entity.getType().name());
+        player.sendMessage(entity.getType().name());
 //        if (set.contains(event.getRightClicked().getType().name())) {
-        if(true){
-            if (!controlEn.containsKey(player)) {
-                controlEn.put(player, entity);
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        if (!controlEn.containsKey(player)) {
-                            controlEn.remove(player);
-                            cancel();
-                        } else {
-                            Vector vector = player.getLocation().getDirection();
-                            Location teleLocation = player.getLocation().add(vector.multiply(3));
-                            entity.teleport(teleLocation);
-                            entity.setFallDistance(0);
-                        }
+
+        if (!controlEn.containsKey(player)) {
+            controlEn.put(player, entity);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (!controlEn.containsKey(player)) {
+                        controlEn.remove(player);
+                        cancel();
+                    } else {
+                        Vector vector = player.getLocation().getDirection();
+                        Location teleLocation = player.getLocation().add(vector.multiply(3));
+                        entity.teleport(teleLocation);
+                        entity.setFallDistance(0);
                     }
-                }.runTaskTimer(plugin, 0, 1);
-            } else {
-                controlEn.remove(player);
-            }
+                }
+            }.runTaskTimer(plugin, 0, 1);
+        } else {
+            controlEn.remove(player);
         }
+
     }
 }
