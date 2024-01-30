30e8f168a4bb7bf246391fbf9695b2f367a20b0b
RiceQuakes <2486921393@qq.com>
2024-01-30 16:37:10 +0800

    add horizontal string

--- src/main/java/lqw/plugintest/MapInteractions/ChainElevator.java
+++ src/main/java/lqw/plugintest/MapInteractions/ChainElevator.java
@@ -1,9 +1,11 @@
 package lqw.plugintest.MapInteractions;
 
 import lqw.plugintest.PluginTest;
+import org.bukkit.Axis;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Chain;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -24,27 +26,77 @@ public class ChainElevator implements Listener {
     @EventHandler
     public void onPlayerRightClickChainBlock(PlayerInteractEvent event) {
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getPlayer().isFlying()) return;
         if (event.getClickedBlock().getType() != Material.CHAIN) return;
         Player player = event.getPlayer();
         Block chain = event.getClickedBlock();
 
-        Location minecartLoc = chain.getLocation();
-//        minecartLoc.add(new Vector(0.5, 0, 0.5));
-        Entity vehicle = minecartLoc.getWorld().spawnEntity(minecartLoc, EntityType.SNOWBALL);
-        vehicle.addPassenger(player);
-        vehicle.setGravity(false);
+        Location ballLoc = chain.getLocation();
+        Chain chainData = (Chain) chain.getBlockData();
+        Axis axis = chainData.getAxis();
+        if (axis != Axis.Y) {
+            ballLoc.setY(ballLoc.getY() - 2);
+            ballLoc.setX(ballLoc.getX() + 0.5);
+            ballLoc.setZ(ballLoc.getZ() + 0.5);
+        }
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                Location location = vehicle.getLocation();
-                vehicle.setVelocity(new Vector(0, 0.4, 0));
-                if (location.getBlock().getType() != Material.CHAIN || vehicle.getPassengers().isEmpty()) {
-                    vehicle.remove();
-                    player.setVelocity(player.getLocation().getDirection().multiply(0.5));
-                    cancel();
+        Entity ball = ballLoc.getWorld().spawnEntity(ballLoc, EntityType.SNOWBALL);
+        ball.addPassenger(player);
+        ball.setGravity(false);
+
+        if (axis == Axis.Y) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    Location location = ball.getLocation();
+                    ball.setVelocity(new Vector(0, 0.4, 0));
+                    if (location.getBlock().getType() != Material.CHAIN || ball.getPassengers().isEmpty()) {
+                        ball.remove();
+                        player.setVelocity(player.getLocation().getDirection().multiply(0.5));
+                        cancel();
+                    }
                 }
-            }
-        }.runTaskTimer(plugin, 0, 1);
+            }.runTaskTimer(plugin, 0, 1);
+        } else if (axis == Axis.X) {
+            Vector vec = player.getLocation().getDirection();
+            vec.setY(0);
+            vec.setZ(0);
+            vec.normalize();
+            vec.multiply(0.5);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    ball.setVelocity(vec);
+                    Location location = ball.getLocation();
+                    location.setY(location.getY() + 2);
+                    location.setX(location.getX() + vec.getX() * 2);
+                    if (location.getBlock().getType() != Material.CHAIN || ball.getPassengers().isEmpty()) {
+                        ball.remove();
+                        player.setVelocity(player.getLocation().getDirection().multiply(0.5));
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(plugin, 0, 1);
+        } else {
+            Vector vec = player.getLocation().getDirection();
+            vec.setY(0);
+            vec.setX(0);
+            vec.normalize();
+            vec.multiply(0.5);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    ball.setVelocity(vec);
+                    Location location = ball.getLocation();
+                    location.setY(location.getY() + 2);
+                    location.setZ(location.getZ() + vec.getZ() * 2);
+                    if (location.getBlock().getType() != Material.CHAIN || ball.getPassengers().isEmpty()) {
+                        ball.remove();
+                        player.setVelocity(player.getLocation().getDirection().multiply(0.5));
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(plugin, 0, 1);
+        }
     }
 }
