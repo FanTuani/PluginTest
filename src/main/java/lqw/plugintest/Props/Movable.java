fc3e09fcdbe0e0aee341db682622175d23de9ca9
ZuiLeiTianShan <766190777@qq.com>
2024-01-30 21:52:54 +0800

    Update Movable.java

--- src/main/java/lqw/plugintest/Props/Movable.java
+++ src/main/java/lqw/plugintest/Props/Movable.java
@@ -18,10 +18,11 @@ import org.bukkit.util.Vector;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public abstract class Movable implements Listener {
-    protected static HashMap<Player, Entity> controlEn = new HashMap<>();
-    protected static PluginTest plugin = PluginTest.pluginTest;
+    protected static HashMap<UUID, Entity> controlEn = new HashMap<>();
+    public static PluginTest plugin = PluginTest.pluginTest;
     protected String blockName, entityName;
 
     public Entity entity;
@@ -31,20 +32,21 @@ public abstract class Movable implements Listener {
         if (event.getHand() == EquipmentSlot.OFF_HAND) return;
         Player player = event.getPlayer();
         Block block = event.getClickedBlock();
+        if(block == null)return;
         if (block.getType().name().equals(blockName)) {
             block.setType(Material.AIR);
-            if (!controlEn.containsKey(player)) {
+            if (!controlEn.containsKey(player.getUniqueId())) {
                 entity = player.getWorld().spawnEntity(player.getLocation() ,EntityType.valueOf(entityName));
 
                     player.sendMessage("1");
                     entity.setVelocity(new Vector(0,1,0));
-                controlEn.put(player, entity);
+                controlEn.put(player.getUniqueId(), entity);
                 new BukkitRunnable() {
                     @Override
                     public void run() {
                             player.sendMessage("1");
-                        if (!controlEn.containsKey(player)) {
-                            controlEn.remove(player);
+                        if (!controlEn.containsKey(player.getUniqueId())) {
+                            controlEn.remove(player.getUniqueId());
                             cancel();
                         } else {
                             Vector vector = player.getLocation().getDirection();
@@ -53,7 +55,7 @@ public abstract class Movable implements Listener {
                             entity.setFallDistance(0);
                         }
                     }
-                }.runTaskTimer(plugin, 0, 1);
+                }.runTaskTimer(PluginTest.pluginTest, 0, 1);
             }
         }
     }
@@ -61,8 +63,8 @@ public abstract class Movable implements Listener {
     @EventHandler
     public void giveUP(PlayerInteractAtEntityEvent event){
         Player player = event.getPlayer();
-        if(controlEn.containsKey(player)){
-            controlEn.remove(player);
+        if(controlEn.containsKey(player.getUniqueId())){
+            controlEn.remove(player.getUniqueId());
             effect(player);
         }
     }
