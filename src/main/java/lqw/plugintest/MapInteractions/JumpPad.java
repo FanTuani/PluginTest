4c1525839ba3a9101dc965fbfccc6c5b1b576e36
RiceQuakes <2486921393@qq.com>
2024-01-30 17:09:29 +0800

    Update JumpPad.java

--- src/main/java/lqw/plugintest/MapInteractions/JumpPad.java
+++ src/main/java/lqw/plugintest/MapInteractions/JumpPad.java
@@ -9,22 +9,21 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
-import javax.swing.*;
-
 public class JumpPad implements Listener {
     public final PluginTest plugin;
 
     public JumpPad(PluginTest plugin) {
         this.plugin = plugin;
     }
+
     @EventHandler
-    public void onPlayerStepOnGoldenPressurePlate(PlayerInteractEvent step){
-        if(step.getAction() == Action.PHYSICAL) {
-            if(step.getClickedBlock().getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE){
+    public void onPlayerStepOnGoldenPressurePlate(PlayerInteractEvent step) {
+        if (step.getAction() == Action.PHYSICAL) {
+            if (step.getClickedBlock().getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE) {
                 Player player = step.getPlayer();
-                Vector nowv = player.getVelocity();
-                nowv.setY(1.8);
-                player.setVelocity(nowv);
+                Vector vec = player.getLocation().getDirection();
+                vec.setY(2);
+                player.setVelocity(vec);
             }
         }
     }
