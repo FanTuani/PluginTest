61212a13017a778f565516190e05dfef6ffc4050
RiceQuakes <2486921393@qq.com>
2024-01-31 12:24:37 +0800

    reformat

--- src/main/java/lqw/plugintest/Props/Movable.java
+++ src/main/java/lqw/plugintest/Props/Movable.java
@@ -22,24 +22,28 @@ public abstract class Movable implements Listener {
     protected static HashMap<UUID, Entity> controlEn = new HashMap<>();
     public static PluginTest plugin = PluginTest.pluginTest;
     protected String blockName, entityName;
-    void effect(Player player){
-        controlEn.get(player.getUniqueId()) .setVelocity(player.getLocation().getDirection());
-        controlEn.get(player.getUniqueId()) .setGravity(true);
+
+    void effect(Player player) {
+        controlEn.get(player.getUniqueId()).setVelocity(player.getLocation().getDirection());
+        controlEn.get(player.getUniqueId()).setGravity(true);
     }
-    void entitySpawn(Player player){
-        Entity entity = player.getWorld().spawnEntity(player.getLocation().add(player.getLocation().getDirection().multiply(3)) ,EntityType.valueOf(entityName));
+
+    void entitySpawn(Player player) {
+        Entity entity =
+                player.getWorld().spawnEntity(player.getLocation().add(player.getLocation().getDirection().multiply(3)), EntityType.valueOf(entityName));
         controlEn.put(player.getUniqueId(), entity);
     }
+
     @EventHandler
     public void pickUp(PlayerInteractEvent event) {
         if (event.getHand() == EquipmentSlot.OFF_HAND) return;
-        if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             Player player = event.getPlayer();
             Block block = event.getClickedBlock();
-            if(block == null)return;
-                if (!controlEn.containsKey(player.getUniqueId())) {
-                    if (block.getType().name().equals(blockName)) {
-                        block.setType(Material.AIR);
+            if (block == null) return;
+            if (!controlEn.containsKey(player.getUniqueId())) {
+                if (block.getType().name().equals(blockName)) {
+                    block.setType(Material.AIR);
                     entitySpawn(player);
                     new BukkitRunnable() {
                         @Override
@@ -58,10 +62,9 @@ public abstract class Movable implements Listener {
                     }.runTaskTimer(PluginTest.pluginTest, 0, 1);
                 }
             }
-        }
-        else{
+        } else {
             Player player = event.getPlayer();
-            if(controlEn.containsKey(player.getUniqueId())){
+            if (controlEn.containsKey(player.getUniqueId())) {
                 effect(player);
                 controlEn.remove(player.getUniqueId());
             }
