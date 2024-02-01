8a63eb9f3cc82b4c82c121caf4b853ba8812cdc6
ZuiLeiTianShan <766190777@qq.com>
2024-02-01 10:24:28 +0800

    CashBox

---
+++ src/main/java/lqw/plugintest/Props/CashBox.java
@@ -0,0 +1,22 @@
+package lqw.plugintest.Props;
+
+import lqw.plugintest.PluginTest;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class CashBox extends Movable implements Listener {
+    public CashBox(){
+        blockName = Material.YELLOW_SHULKER_BOX.name();
+    }
+    void entitySpawn(Player player) {
+        FallingBlock fallingBlock = player.getWorld().spawnFallingBlock(player.getLocation().add(player.getLocation().getDirection()),Material.STONE.createBlockData());
+        fallingBlock.setDropItem(false);
+        fallingBlock.setGravity(false);
+        fallingBlock.setHurtEntities(false);
+        controlEn.put(player.getUniqueId(), fallingBlock);
+    }
+}
