bdd5c5817d2d65f14913450936443462513c51b6
ZuiLeiTianShan <766190777@qq.com>
2024-02-02 20:56:25 +0800

    GiveUpCashBox

--- src/main/java/lqw/plugintest/Props/CashBox.java
+++ src/main/java/lqw/plugintest/Props/CashBox.java
@@ -1,23 +1,25 @@
 package lqw.plugintest.Props;
 
-import lqw.plugintest.PluginTest;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.FallingBlock;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.Listener;
 
 public class CashBox extends Movable implements Listener {
-    public CashBox(){
+    public CashBox() {
         blockName = Material.GOLD_BLOCK.name();
     }
+
     void entitySpawn(Player player) {
-        FallingBlock fallingBlock = player.getWorld().spawnFallingBlock(player.getLocation().add(player.getLocation().getDirection()),Material.GOLD_BLOCK.createBlockData());
-        fallingBlock.setDropItem(false);
-        fallingBlock.setGravity(false);
-        fallingBlock.setHurtEntities(false);
-        controlEn.put(player.getUniqueId(), fallingBlock);
+//        FallingBlock fallingBlock = player.getWorld().spawnFallingBlock(player.getLocation().add(player.getLocation().getDirection()), Material.GOLD_BLOCK.createBlockData());
+//        fallingBlock.setDropItem(false);
+//        fallingBlock.setGravity(false);
+//        fallingBlock.setHurtEntities(false);
+//        controlEn.put(player.getUniqueId(), fallingBlock);
+        Entity entity =
+                player.getWorld().spawnEntity(player.getLocation(), EntityType.valueOf(EntityType.SHULKER.name()));
+        Shulker shulker = (Shulker) entity;
+        shulker.setAI(false);
+        controlEn.put(player.getUniqueId(), entity);
     }
 
     @Override
---
+++ src/main/java/lqw/plugintest/Props/Hook.java
@@ -0,0 +1,9 @@
+package lqw.plugintest.Props;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class Hook implements Listener {
+    @EventHandler
+    
+}
